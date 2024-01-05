c44cc331c7a32e68419ae347c4753a42dcbe3f1a
JosmanAltamira <josman_altamira@hotmail.com>
2024-01-05 03:44:46 -0600

    cambio de ruta

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // baseUrl: 'http://localhost/backendTra/public/api',
-  baseUrl: 'http://192.168.100.138/crud_api/alumnos',
+  baseUrl: 'http://localhost/backendTra/public/api',
+  // baseUrl: 'http://192.168.100.138/crud_api/alumnos',
 };
 
 /*
